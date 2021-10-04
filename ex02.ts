3eb26cd9ca066a6457cabe27cf5c607810afaf65
Croniums <valentin.thomas@efrei.net>
2021-10-04 11:04:45 +0200

    ex02

---
+++ ex02.ts
@@ -0,0 +1,16 @@
+function uppercase(str = ''): string{
+    let result = ''
+    for (let i =0; i < str.length; i++) {
+        const code = str.charCodeAt(i)
+        if (code >= 97 && code <= 122) {
+            result += String.fromCharCode(code - 32)
+        } else {
+            result += str[i]
+        }
+    }
+    return result
+}
+
+console.log(uppercase('Chopper'))
+console.log(uppercase('TONY TONY'))
+console.log(uppercase('BonjouR'))

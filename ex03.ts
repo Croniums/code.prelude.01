cb506ce9c8f91153139b8c2b36fb6fa257bd29d5
Croniums <valentin.thomas@efrei.net>
2021-10-04 11:12:06 +0200

    ex03

---
+++ ex03.ts
@@ -0,0 +1,9 @@
+function lengthh(len = ''): string {
+    let result: any = 0
+    for (let i=0;i < len.length;i++) {
+        result = result + 1
+
+    }
+    return result
+}
+console.log(lengthh('Ch0pper'))
